e7bfe85f39df1a70f41a15c8384b7ab4512c5175
hrspx <bklimowski1@gmail.com>
2022-02-21 19:23:49 +0100

    update

--- gitexplorer/containers.py
+++ gitexplorer/containers.py
@@ -4,29 +4,30 @@ from github import Github
 from . import SearchServices
 from . import SaveServices
 
+def _get_avilable_services(config):
+    services_names = [
+        ii.service_name for ii in SaveServices.SaveServiceInterface.__subclasses__()
+    ]
+    services_factories = [
+        providers.Factory(jj, db_path=config.db.path)
+        for jj in SaveServices.SaveServiceInterface.__subclasses__()
+    ]
+    return dict(zip(services_names, services_factories))
 class Container(containers.DeclarativeContainer):
-
-    avilable_save_services = dict(zip(
-            [ii.service_name for ii in SaveServices.SaveServiceInterface.__subclasses__()],
-            SaveServices.SaveServiceInterface.__subclasses__()))
-
-    print(avilable_save_services)
 
     wiring_config = containers.WiringConfiguration(modules=[".views"])
     config = providers.Configuration(yaml_files=["config.yml"])
 
     github_client = providers.Factory(
         Github,
-        login_or_token = config.github.auth_token,
-        timeout = config.github.request_timeout
+        login_or_token=config.github.auth_token,
+        timeout=config.github.request_timeout,
     )
 
     search_service = providers.Factory(
-        SearchServices.SearchService,
-        github_client = github_client
+        SearchServices.SearchService, github_client=github_client
     )
 
-    save_service = providers.Factory(
-        avilable_save_services["pickle"],
-        db_path = "pickles_folder" 
+    save_service = providers.Selector(
+        config.db.engine, **_get_avilable_services(config)
     )
